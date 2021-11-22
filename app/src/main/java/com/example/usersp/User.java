9056b34eb183a7e19930b6528a0b7b945e6b5072
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2021-11-22 07:16:22 -0600

    Diseño de RecycleView Data Class

---
+++ app/src/main/java/com/example/usersp/User.java
@@ -0,0 +1,15 @@
+package com.cursosant.android.usersp
+
+/****
+ * Project: Fundamentos Kotlin
+ * From: com.cursosant.android.fundamentoskotlin.classes
+ * Created by Mariana Sanchez on 10/11/20 at 13:17
+ * Course: Android Practical whit Kotlin from zero.
+ * All rights reserved 2021
+ *
+ * All my Courses(Only  on Udemy ):
+ * https://www.udemy.com/user/alain-nicolas-tello/
+ ****/
+data class User (val id: long, var name:String, var lastName: String,  var url:String){
+
+}
