04497120dcc72106cbe3def67fc1f7ade6e8428f
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2021-11-22 21:23:22 -0600

    Recycler View : Configuración

--- app/src/main/java/com/example/usersp/MainActivity.kt
+++ app/src/main/java/com/example/usersp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.usersp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        userAdapter = UserAdapter(mutableListOf())
+        linearLayoutManager= LinearLayoutManager(context: this)
+        binding.recyclerView.apply{ this: RecyclerView
+            LayoutManager= linearLayoutManager
+            adapter= userAdapter
+
+        }
     }
 }
